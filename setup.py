4fbbecdcb0a07c34cfdf3726daed5d1ce274b891
vivek <vivekrockss111@gmail.com>
2025-12-14 18:33:17 +0530

    Setup.py file is readyy

--- setup.py
+++ setup.py
@@ -1,6 +1,43 @@
-python-dotenv
-pandas
-numpy
-matplotlib
-scikit-learn
+"""
+The setup.py file is crucial for any project, as it is an essential
+part of packaging and distributing Python projects. 
+It is used by Setuptools to define the configuration of a project,
+such as its metadata, dependencies, and more.
+"""
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    :return: Description
+    :rtype: List[str]
+    """
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines=file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_list.append(requirement)
+    
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_list
+
+
+setup(
+    name = "NetworkSecurity",
+    version="0.0.1",
+    author="Vivek Sharma",
+    author_email="vivekshar2000@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+
+)
+
 
